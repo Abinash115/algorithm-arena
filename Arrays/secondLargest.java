9b994566081ddb8acca05f92abf6d0cb1595debe
Abinash115 <patsaniabninash@gmail.com>
2026-09-08 23:54:31 +0530

    Second Largest Element in an array

---
+++ Arrays/secondLargest.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class secondLargest {
+   
+   public static int secondLargest(int[] arr){
+      int largest = Integer.MIN_VALUE;
+      int secondLaegest = Integer.MIN_VALUE;
+
+      for(int i = 0; i < arr.length; i++){
+          if(arr[i] > largest){
+              secondLaegest = largest;
+              largest = arr[i];
+          }else if(arr[i] > secondLaegest && arr[i] != largest){
+              secondLaegest = arr[i];
+          }
+      }
+      return secondLaegest;
+   }
+   public static void main(String[] args) {
+      int arr[] = {1, 2, 4, 7, 7, 5};
+      System.out.println(secondLargest(arr));  // 5  Time complexity : O(N)  Space complexity : O(1)
+   }
+}
+
+// https://www.geeksforgeeks.org/problems/second-largest3735/1
