95a52c06b539c493d1b383c9e69c36d073fe0246
Abinash115 <patsaniabninash@gmail.com>
2026-09-11 08:39:10 +0530

    pushing leet code problem description for ceil in sorted array

--- BinarySearch/floorCeilBinarySearch.java
+++ BinarySearch/floorCeilBinarySearch.java
@@ -46,3 +46,4 @@ public class floorCeilBinarySearch {
 }
 
 // https://www.geeksforgeeks.org/problems/floor-in-a-sorted-array-1587115620/1
+// https://www.geeksforgeeks.org/problems/ceil-in-a-sorted-array/1
