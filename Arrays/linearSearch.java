84018ae73bd191c7ae2b72fe14202c387441df75
Abinash115 <patsaniabninash@gmail.com>
2026-09-11 08:31:45 +0530

    Linear Search

---
+++ Arrays/linearSearch.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class linearSearch {
+   public static int linearSearch(int[] arr, int target){
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == target){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5, 6, 7};
+        int target = 5;
+        System.out.println(linearSearch(arr, target)); // 4 Time complexity : O(N)  Space complexity : O(1)
+    }
+}
+
+// 
